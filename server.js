a0bfa3fa55098d27d3edd36a4521384f7b6e6f16
Rodrigo Becker Probst <becker.rodrigo@outlook.com>
2018-11-11 17:11:48 -0200

    Commit Inicial

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 const app = express();
+app.use(express.json);
+
+mongoose.connect('mongodb://localhost:27017/NodeJS', {
+    useNewUrlParser: true
+});
+
+requireDir('./src/models');
+
+app.use("/api", require("./src/routes"));
 
 app.listen(3002);
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
-});
+console.log('Running on server :3002');
+
+
